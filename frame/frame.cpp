eb595fe78cac16bd54aad027261d34dcbf874c20
Mahmoud Ehab <mahmoud.ehab.moheb.dev@gmail.com>
2026-04-19 17:36:12 +0200

    Formatting code

--- frame/frame.cpp
+++ frame/frame.cpp
@@ -4,31 +4,35 @@
 #include <vector>
 using namespace std;
 
-void Frame::display(ostream& out) const {
+void Frame::display(ostream &out) const
+{
     string typeStr = (type == DATA) ? "DATA" : "ACK";
     out << "[" << typeStr << " | Seq: " << seq_no
         << " | Ack: " << ack_no << " | Checksum: " << checksum << "]";
 }
-ostream& operator <<(std::ostream& out, const Frame& aFrame) {
+ostream &operator<<(std::ostream &out, const Frame &aFrame)
+{
     aFrame.display(cout);
     return cout;
 }
 
- 
-
-int FrameArchitect:: calculateChecksum(FrameType type, int seq, int ack, const std::string& data) {
+int FrameArchitect::calculateChecksum(FrameType type, int seq, int ack, const std::string &data)
+{
     int sum = (int)type + seq + ack;
-    for (char c : data) {
+    for (char c : data)
+    {
         sum += (int)c;
     }
     return sum;
 }
 
-bool FrameArchitect::isValid(const Frame& f) {
+bool FrameArchitect::isValid(const Frame &f)
+{
     return f.checksum == calculateChecksum(f.type, f.seq_no, f.ack_no, f.payload);
 }
 
-Frame FrameArchitect::createFrame(FrameType type, int seq, int ack, std::string data) {
+Frame FrameArchitect::createFrame(FrameType type, int seq, int ack, std::string data)
+{
     Frame newFrame;
     newFrame.type = type;
     newFrame.seq_no = seq;
@@ -39,21 +43,27 @@ Frame FrameArchitect::createFrame(FrameType type, int seq, int ack, std::string 
 }
 
 //
-void SenderBuffer::addToBuffer(const Frame& f) {
+void SenderBuffer::addToBuffer(const Frame &f)
+{
     windowBuffer.push_back(f);
 }
 
-Frame SenderBuffer::getFrameFromBuffer(int seq) {
-    for (const auto& f : windowBuffer) {
-        if (f.seq_no == seq) return f;
+Frame SenderBuffer::getFrameFromBuffer(int seq)
+{
+    for (const auto &f : windowBuffer)
+    {
+        if (f.seq_no == seq)
+            return f;
     }
-    return {}; // Return empty 
+    return {}; // Return empty
 }
 
-void SenderBuffer::clearBufferUpTo(int ack_no) {
+void SenderBuffer::clearBufferUpTo(int ack_no)
+{
     // Removes frames that have been successfully acknowledged
     windowBuffer.erase(
-       remove_if(windowBuffer.begin(), windowBuffer.end(),   //:) AI ,don't ask me
-            [ack_no](const Frame& f) { return f.seq_no <= ack_no; }),
+        remove_if(windowBuffer.begin(), windowBuffer.end(), //:) AI ,don't ask me
+                  [ack_no](const Frame &f)
+                  { return f.seq_no <= ack_no; }),
         windowBuffer.end());
 }
--- frame/frame.h
+++ frame/frame.h
@@ -3,48 +3,50 @@
 #include <string>
 #include <vector>
 
+enum FrameType
+{
+    DATA,
+    ACK
+};
 
-enum FrameType { DATA, ACK }; 
-
-struct Frame {
+struct Frame
+{
     FrameType type;
     int seq_no;
     int ack_no;
     std::string payload;
     int checksum;
 
-   
-    void display(ostream& cout) const; //done
+    void display(ostream &cout) const; // done
     // Overloaded output operator
-    friend ostream& operator<<(std::ostream& out, const Frame& aFrame);
+    friend ostream &operator<<(std::ostream &out, const Frame &aFrame);
 };
 
-class FrameArchitect {
+class FrameArchitect
+{
 public:
     //  Checksum Implementation
     // This calculates a simple sum of all integer values and characters in the payload
-    //di 3a4an check  if the message changed or it as same
-    int calculateChecksum(FrameType type, int seq, int ack, const std::string& data);
-
+    // di 3a4an check  if the message changed or it as same
+    int calculateChecksum(FrameType type, int seq, int ack, const std::string &data);
 
     // Function to verify if a received frame is corrupted
-    //here if the receiver take time to send the ACK or didn't send it 
-    bool isValid(const Frame& f);
-
+    // here if the receiver take time to send the ACK or didn't send it
+    bool isValid(const Frame &f);
 
     //  Ay frame sender or el receiver e3malo hnst5dmha 3a4an n check is the message changed or not ?
     Frame createFrame(FrameType type, int seq, int ack, std::string data);
 };
 
-
-//  Buffer 
+//  Buffer
 // This stores frames  in case of a Timeout (copy of the frame and then clear it when transmit successfully :))
-class SenderBuffer {
+class SenderBuffer
+{
 private:
     std::vector<Frame> windowBuffer;
 
 public:
-    void addToBuffer(const Frame& f);
+    void addToBuffer(const Frame &f);
 
     Frame getFrameFromBuffer(int seq);
 
